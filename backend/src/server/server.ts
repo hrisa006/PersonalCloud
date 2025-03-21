69830b2a43b13da1a6219cf50c026958352d28b4
Veselin Radev <veselinradev4@gmail.com>
2025-03-21 13:30:23 +0200

    test commit

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,6 +1,6 @@
 import app from '../app';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80801;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
