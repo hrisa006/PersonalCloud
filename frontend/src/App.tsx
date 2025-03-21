7ddf3d0bc3d21bcbc4272f8531f48c44c3fdeec9
Veselin Radev <veselinradev4@gmail.com>
2025-03-21 18:37:43 +0200

    merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,6 @@ function App() {
   const [data, setData] = useState<{ test: string }[]>([]);
 
   useEffect(() => {
-    const apiUrl = import.meta.env.VITE_URL;
-
     axios.defaults.headers.get["Content-Type"] = "application/json";
 
     axios
