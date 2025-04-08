3369689bd98c01035ecd4133071451b0c623f7da
Veselin Radev <veselinradev4@gmail.com>
2025-04-08 19:44:00 +0300

    cleanup

--- backend/src/controller/file-upload-controller.ts
+++ backend/src/controller/file-upload-controller.ts
@@ -22,35 +22,43 @@ function getUploadPath(filePath: string): string {
     return uploadPath;
 }
 
+function processFile(filename: FileName, res: e.Response<any, Record<string, any>>, filePath: string, file: NodeJS.ReadableStream) {
+    if (!filename) {
+        res.status(400).send('There was an error parsing the file');
+        return;
+    }
+
+    console.log(`Uploading file: ${filename.filename}`);
+
+    const fileToSave = path.join(getUploadPath(filePath), filename.filename);
+    const writeStream = fs.createWriteStream(fileToSave);
+
+    file.pipe(writeStream);
+
+    writeStream.on('close', () => {
+        console.log(`Finished writing ${filename.filename}`);
+    });
+}
+
+function processField(fieldname: string, filePath: string, value: string) {
+    //add path validations
+    if (fieldname === 'filePath') {
+        filePath = value;
+    }
+    return filePath;
+}
+
 export const fileUpload = (req: Request, res: Response) => {
     const bb = busboy({ headers: req.headers });
     let filePath = '';
-
     bb.on('field', (fieldname: string, value: string) => {
-        //add path validations
-        if (fieldname === 'filePath') {
-            filePath = value;
-        }
+        filePath = processField(fieldname, filePath, value);
     });
 
     bb.on('file',
         (fieldname: string, file: NodeJS.ReadableStream, filename: FileName) => {
-        if (!filename) {
-            res.status(400).send('There was an error parsing the file');
-            return;
-        }
-
-        console.log(`Uploading file: ${filename.filename}`);
-
-        const fileToSave = path.join(getUploadPath(filePath) ,filename.filename);
-        const writeStream = fs.createWriteStream(fileToSave);
-
-        file.pipe(writeStream);
-
-        writeStream.on('close', () => {
-            console.log(`Finished writing ${filename.filename}`);
+            processFile(filename, res, filePath, file);
         });
-    });
 
     bb.on('finish', () => {
         res.status(200).send('Upload complete');
