65f1d33472fba6bd45351bbb83c691d7a3518eaf
Veselin Radev <veselinradev4@gmail.com>
2025-04-12 02:25:23 +0300

    remove test controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,11 @@
-import express, {NextFunction} from 'express';
+import express from 'express';
 import cors from 'cors';
-import userRoutes from './route/routes';
 import fileRoutes from './route/file-routes';
 import {badRequestErrorHandler, globalExceptionHandler} from "./middleware/global-exception-handler";
 
 const app = express();
 app.use(express.json());
 app.use(cors<express.Request>());
-app.use('/api/test', userRoutes);
 app.use('/file', fileRoutes);
 
 app.use(badRequestErrorHandler);
--- backend/src/controller/test-controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import {BadRequestError} from "../errors/bad-request-error";
-
-export const test = (req: Request, res: Response) => {
-    res.json([{ 'test': 'test' }]);
-};
